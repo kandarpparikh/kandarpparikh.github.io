5e79f042c345d498cbaa46b406fa0949cefb73c5
Ubuntu <ubuntu@ip-172-31-20-226.ec2.internal>
2022-04-21 04:38:24 +0000

    Updates

---
+++ static/js/4.342ab08f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkandarp-portfolio"]=this["webpackJsonpkandarp-portfolio"]||[]).push([[4],{101:function(t,e,i){"use strict";i.r(e);i(3);var a=i(2);e.default=function(t){return Object(a.jsx)("div",{className:"d-flex justify-content-center align-items-center m-2 icon-card",children:Object(a.jsx)("img",{className:"img-fluid skills-img",src:t.src,alt:t.label,title:t.label,loading:"lazy"})})}}}]);
+//# sourceMappingURL=4.342ab08f.chunk.js.map
